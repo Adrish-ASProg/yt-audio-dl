1b97ab11d01e70e481457678997f875783ab5766
Adrish <Adrieen-s@hotmail.fr>
2020-01-19 12:15:22 +0100

    (backend) added dedicated service to do work

--- backend/src/main/java/com/asoft/ytdl/controller/FileController.java
+++ backend/src/main/java/com/asoft/ytdl/controller/FileController.java
@@ -1,12 +1,11 @@
 package com.asoft.ytdl.controller;
 
-import com.asoft.ytdl.enums.ProgressStatus;
 import com.asoft.ytdl.model.ConvertRequest;
 import com.asoft.ytdl.model.FileStatus;
-import com.asoft.ytdl.utils.DownloadManager;
+import com.asoft.ytdl.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,53 +14,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import static com.asoft.ytdl.utils.FileUtils.getFile;
 
 @RestController
 public class FileController {
 
-    private final Map<String, FileStatus> filesStatus = new HashMap<>();
+    @Autowired
+    ApplicationService applicationService;
 
     @RequestMapping(value = "/convert", method = RequestMethod.POST)
     public ResponseEntity<Object> convert(@RequestBody ConvertRequest convertRequest) {
-        UUID uuid = UUID.randomUUID();
-
-        filesStatus.put(uuid.toString(),
-                new FileStatus() {{
-                    setUuid(uuid.toString());
-                    setName("N/A");
-                    setStatus(ProgressStatus.INITIALIZING);
-                    setStartDate(new Date().getTime());
-                }}
-        );
-
-        DownloadManager ytManager = new DownloadManager() {
-            @Override
-            public void onProgress(ProgressStatus progressStatus) {
-                filesStatus.get(uuid.toString()).setStatus(progressStatus);
-            }
-
-            @Override
-            public void onDownloadCompleted(String fileName) {
-                FileStatus fs = filesStatus.get(uuid.toString());
-                fs.setStatus(ProgressStatus.COMPLETED);
-                fs.setName(fileName);
-            }
-        };
-        ytManager.download(convertRequest.getUrl(), convertRequest.getAudioOnly());
-
+        String uuid = applicationService.convertFile(convertRequest);
         return new ResponseEntity<>(Collections.singletonMap("uuid", uuid), HttpStatus.ACCEPTED);
     }
 
@@ -70,31 +35,7 @@ public class FileController {
     public @ResponseBody
     void download(HttpServletResponse response, @RequestParam(value = "uuid") String uuid)
             throws FileNotFoundException {
-
-        // UUID not found
-        if (!filesStatus.containsKey(uuid)) {
-            throw new FileNotFoundException("Unable to find file with uuid « " + uuid + " »");
-        }
-
-        FileStatus fileStatus = filesStatus.get(uuid);
-
-        // File not downloaded yet
-        if (fileStatus.getStatus() != ProgressStatus.COMPLETED) {
-            throw new FileNotFoundException("File not downloaded yet. Current status: " + fileStatus.getStatus());
-        }
-
-        try {
-            File file = getFile("downloaded/" + fileStatus.getName() + ".mp3");
-            InputStream in = new FileInputStream(file);
-            response.setContentType("audio/mpeg");
-            response.setHeader("Content-Disposition", "attachment; filename=" + file.getName());
-            response.setHeader("Content-Length", String.valueOf(file.length()));
-            response.setHeader("FileName", fileStatus.getName() + ".mp3");
-            FileCopyUtils.copy(in, response.getOutputStream());
-        } catch (IOException e) {
-            System.err.println("IOException, see logs below");
-            e.printStackTrace();
-        }
+        applicationService.downloadFile(uuid, response);
     }
 
 
@@ -102,16 +43,12 @@ public class FileController {
 
     @RequestMapping(value = "/status", method = RequestMethod.GET)
     public ResponseEntity<FileStatus> status(@RequestParam(value = "uuid") String uuid) throws FileNotFoundException {
-        if (!filesStatus.containsKey(uuid)) {
-            throw new FileNotFoundException("Unable to find file with uuid « " + uuid + " »");
-        }
-
-        return new ResponseEntity<>(filesStatus.get(uuid), HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(applicationService.getFileStatus(uuid), HttpStatus.ACCEPTED);
     }
 
     @RequestMapping(value = "/status/all", method = RequestMethod.GET)
     public ResponseEntity<Collection<FileStatus>> statusAll() {
-        return new ResponseEntity<>(this.filesStatus.values(), HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(applicationService.getAllFilesStatus(), HttpStatus.ACCEPTED);
     }
 
     // #endregion
---
+++ backend/src/main/java/com/asoft/ytdl/service/ApplicationService.java
@@ -0,0 +1,97 @@
+package com.asoft.ytdl.service;
+
+import com.asoft.ytdl.enums.ProgressStatus;
+import com.asoft.ytdl.model.ConvertRequest;
+import com.asoft.ytdl.model.FileStatus;
+import com.asoft.ytdl.utils.DownloadManager;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.asoft.ytdl.utils.FileUtils.getFile;
+
+@Service
+public class ApplicationService {
+
+    private final Map<String, FileStatus> filesStatus = new HashMap<>();
+
+    public String convertFile(ConvertRequest convertRequest) {
+        UUID uuid = UUID.randomUUID();
+
+        filesStatus.put(uuid.toString(),
+                new FileStatus() {{
+                    setUuid(uuid.toString());
+                    setName("N/A");
+                    setStatus(ProgressStatus.INITIALIZING);
+                    setStartDate(new Date().getTime());
+                }}
+        );
+
+        DownloadManager ytManager = new DownloadManager() {
+            @Override
+            public void onProgress(ProgressStatus progressStatus) {
+                filesStatus.get(uuid.toString()).setStatus(progressStatus);
+            }
+
+            @Override
+            public void onDownloadCompleted(String fileName) {
+                FileStatus fs = filesStatus.get(uuid.toString());
+                fs.setStatus(ProgressStatus.COMPLETED);
+                fs.setName(fileName);
+            }
+        };
+        ytManager.download(convertRequest.getUrl(), convertRequest.getAudioOnly());
+
+        return uuid.toString();
+    }
+
+    public FileStatus getFileStatus(String uuid) throws FileNotFoundException {
+        if (!filesStatus.containsKey(uuid)) {
+            throw new FileNotFoundException("Unable to find file with uuid « " + uuid + " »");
+        }
+
+        return filesStatus.get(uuid);
+    }
+
+    public Collection<FileStatus> getAllFilesStatus() {
+        return this.filesStatus.values();
+    }
+
+    public void downloadFile(String uuid, HttpServletResponse response) throws FileNotFoundException {
+        // UUID not found
+        if (!filesStatus.containsKey(uuid)) {
+            throw new FileNotFoundException("Unable to find file with uuid « " + uuid + " »");
+        }
+
+        FileStatus fileStatus = filesStatus.get(uuid);
+
+        // File not downloaded yet
+        if (fileStatus.getStatus() != ProgressStatus.COMPLETED) {
+            throw new FileNotFoundException("File not downloaded yet. Current status: " + fileStatus.getStatus());
+        }
+
+        try {
+            File file = getFile("downloaded/" + fileStatus.getName() + ".mp3");
+            InputStream in = new FileInputStream(file);
+            response.setContentType("audio/mpeg");
+            response.setHeader("Content-Disposition", "attachment; filename=" + file.getName());
+            response.setHeader("Content-Length", String.valueOf(file.length()));
+            response.setHeader("FileName", fileStatus.getName() + ".mp3");
+            FileCopyUtils.copy(in, response.getOutputStream());
+        } catch (IOException e) {
+            System.err.println("IOException, see logs below");
+            e.printStackTrace();
+        }
+    }
+}
