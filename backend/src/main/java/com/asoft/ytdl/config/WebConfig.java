301a8b5ce11d876e158aa5467daf237d922ec342
Adrish <Adrieen-s@hotmail.fr>
2020-02-16 19:47:15 +0100

    back: fixed redirect from root url not working

--- backend/src/main/java/com/asoft/ytdl/config/WebConfig.java
+++ backend/src/main/java/com/asoft/ytdl/config/WebConfig.java
@@ -3,14 +3,12 @@ package com.asoft.ytdl.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
 
-@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
